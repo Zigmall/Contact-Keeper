798d5b0e0a7346569782061eb515978c4f3bae66
Zigmal <rafal.hes@yahoo.com>
2021-10-27 22:09:08 +0200

    Add global state

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import { faIdCardAlt } from '@fortawesome/free-solid-svg-icons';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import ContactState from './context/contact/ContactState';
 
 
 
@@ -14,17 +15,19 @@ library.add(fab, faIdCardAlt );
 
 const App = () => {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <div className='container'>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-          </Switch>
-        </div>
-      </>
-    </Router>
+    <ContactState>
+      <Router>
+        <>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </>
+      </Router>
+    </ContactState>
   );
 };
 
---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,61 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_CONTACT,
+    FILTER_CONTACT,
+    CLEAR_FILTER
+} from '../types';
+
+const ContactState = props => {
+    const initialState = {
+        contacts: [
+            {
+                id: 1,
+                name: "Lara Croft",
+                email: "lcroft@gmail.com",
+                phone: "123-333-123", 
+                type:"personal"
+            }, 
+            {
+                id: 2,
+                name: "Mark Leric",
+                email: "ml@gmail.com",
+                phone: "333-333-333", 
+                type:"personal"
+            },
+        ] 
+    }
+
+    const [state, dispatch] = useReducer(contactReducer, initialState);
+
+    //Add contact
+
+    //Delete contact
+
+    //Set current contacft
+
+    //Clear current contact
+
+    //Update contact
+
+    //Filter contact
+
+    //Clear filter
+
+    return (
+        <ContactContext.Provider
+        value={{
+            contacts: state.contacts
+        }}>
+            {props.children}
+        </ContactContext.Provider>
+    )
+}
+
+export default ContactContext;
