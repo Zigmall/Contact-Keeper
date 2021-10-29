6888e6c17d1eb724b1610d5846626fdcd33c61bd
Zigmal <rafal.hes@yahoo.com>
2021-10-29 12:45:36 +0200

    Work on ADD_CONTACT in reducer

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import ContactContext from '../../context/contact/contactContext';
 
 const ContactForm = () => {
+  const contactContext = useContext(ContactContext);
+
   const [contact, setContact] = useState({
     name: '',
     email: '',
@@ -10,37 +13,65 @@ const ContactForm = () => {
 
   const { email, name, phone, type } = contact;
 
+  const onChange = (e) =>
+    setContact({ ...contact, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    contactContext.addContact(contact);
+    setContact({
+      name: '',
+      email: '',
+      phone: '',
+      type: 'personal'
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h2 className="text-primary">Add Contact</h2>
       <input
         type="text"
         placeholder="Name"
-        name="neme"
-        value={neme}
-        onChange={onChange }
+        name="name"
+        value={name}
+        onChange={onChange}
       />
       <input
         type="email"
         placeholder="Email"
         name="email"
         value={email}
-        onChange={onChange }
+        onChange={onChange}
       />
       <input
         type="text"
         placeholder="Phone"
         name="phone"
         value={phone}
-        onChange={onChange }
+        onChange={onChange}
       />
       <h5>Contact Type</h5>
-      <input type='radio' neme='type' value='personal' checked={input === 'personal'} />
-      personal{' '}
-      <input type='radio' neme='type' value='professional' checked={input === 'professional'} /> Professional
-      personal{' '}
+      <input
+        type="radio"
+        neme="type"
+        value="personal"
+        checked={type === 'personal'}
+      />
+      Personal{' '}
+      <input
+        type="radio"
+        neme="type"
+        value="professional"
+        checked={type === 'professional'}
+      />{' '}
+      Professional{' '}
       <div>
-          <input type='submit' value='Add Contact' className='btn btn-primary btn-block' />
+        <input
+          type="submit"
+          value="Add Contact"
+          className="btn btn-primary btn-block"
+        />
       </div>
     </form>
   );
