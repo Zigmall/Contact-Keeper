0836172d4c3c800c7167c6dafd07a641da747493
Zigmal <rafal.hes@yahoo.com>
2021-10-31 18:23:46 +0100

    Add Login and Register components

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+
+const Register = () => {
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const { name, email, password, password2 } = user;
+  const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
+  const onSubmit = e => {
+      e.preventDefault();
+      console.log('Register submit');
+  }
+  return (
+    <div className='form-container'>
+      <h1>
+        Account <span className='text-primary'>Register</span>
+      </h1>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <lanel htmlFor='name'>Name</lanel>
+          <input
+            type='text'
+            name='name'
+            value={name}
+            onChange={onChange}
+          ></input>
+        </div>
+        <div className='form-group'>
+          <lanel htmlFor='email'>Email Adress</lanel>
+          <input
+            type='email'
+            name='email'
+            value={email}
+            onChange={onChange}
+          ></input>
+        </div>
+        <div className='form-group'>
+          <lanel htmlFor='passowrd'>Password</lanel>
+          <input
+            type='passowrd'
+            name='password'
+            value={password}
+            onChange={onChange}
+          ></input>
+        </div>
+        <div className='form-group'>
+          <lanel htmlFor='passowrd2'>Confirm Password</lanel>
+          <input
+            type='passowrd'
+            name='password2'
+            value={password2}
+            onChange={onChange}
+          ></input>
+        </div>
+        <input type='submit' value='Register' className='btn btn-primary btn-block' />
+      </form>
+    </div>
+  );
+};
+
+export default Register;
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -7,45 +7,46 @@ import {
     USER_LOADED,
     AUTH_ERROR,
     LOGIN_SUCCESS,
-    LOGIN_FAIL, 
+    LOGIN_FAIL,
     LOGOUT,
-    CLEAR_ERRORS,
-} from '../types';
+    CLEAR_ERRORS
+  } from '../types';
 
 const AuthState = props => {
     const initialState = {
-       token: localStorage.getItem('token'),
-       isAuthenticated: null,
-       loading: true,
-       user: null,
-       error: null
-    }
+      token: localStorage.getItem('token'),
+      isAuthenticated: null,
+      loading: true,
+      user: null,
+      error: null
+    };
 
     const [state, dispatch] = useReducer(authReducer, initialState);
 
-    //Load User
+  //Load User
 
-    //Register User
+  //Register User
 
-    //Login user
+  //Login user 
 
-    //Logout
+  //Logout
 
-    //Clear Errors
+  //Clear Errors
 
-
-    return (
-        <AuthContext.Provider
-        value={{
-           token: state.token,
-           isAuthenticated: state.isAuthenticated,
-           loading: state.loading, 
-           user: state.user,
-           error: state.error
-        }}>
-            {props.children}
-        </AuthContext.Provider>
-    )
-}
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        user: state.user,
+        error: state.error 
+     
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthState;
