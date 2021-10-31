2f4adc0beab5b0da83c6f3b9745965f2ef26c68a
Zigmal <rafal.hes@yahoo.com>
2021-10-31 11:39:16 +0100

    Add auth context and state

--- client/src/App.js
+++ client/src/App.js
@@ -3,31 +3,36 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faIdCardAlt, faEnvelopeOpen, faPhone } from '@fortawesome/free-solid-svg-icons';
+import {
+  faIdCardAlt,
+  faEnvelopeOpen,
+  faPhone,
+} from '@fortawesome/free-solid-svg-icons';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
 
-
-
-library.add(fab, faIdCardAlt, faEnvelopeOpen,faPhone );
+library.add(fab, faIdCardAlt, faEnvelopeOpen, faPhone);
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <>
-          <Navbar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <>
+            <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+              </Switch>
+            </div>
+          </>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 };
 
